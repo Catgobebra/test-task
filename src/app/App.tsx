b5d269ccd0fb87d39c9faef89b0d20e291b4917a
Catgobebra <elderlyjmih@gmail.com>
2026-01-27 17:30:16 +0500

    feat: add react router and basic routing setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import {EmployeeDetailPage} from '../pages/EmployeeListPage'
+import {EmployeeListPage} from '../pages/EmployeeListPage'
+import {NotFound} from '../pages/NotFound'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+function App() {
+  return (
+   <BrowserRouter>
+      <Routes>
+        <Route path="/employees" element={<EmployeeDetailPage />} />
+        <Route path="/employees/:id" element={<EmployeeListPage />}  />
+      </Routes>
+      <Route path="*" element={<NotFound />} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
