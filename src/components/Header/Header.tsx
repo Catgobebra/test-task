2afe8e0f9978bd2339a323afe37ec707276a5990
Catgobebra <elderlyjmih@gmail.com>
2026-01-28 19:33:57 +0500

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { ToggleSwitch } from '../../modules/ThemeSwitch'
+import Logo from '../../assets/Логотип 66бит 1.svg'
+
+function Header() {
+  return (
+    <header className='w-full shadow-lg shadow-[#main-bg-dark]-500/50'>
+      <div className='container mx-auto px-4 md:px-6 h-13.5 md:h-22.5 flex justify-between items-center'>
+        <img
+          src={Logo} 
+          className='w-20 md:w-25.25 h-auto' 
+          alt="Logo" 
+        />   
+        <div className='flex items-center gap-4 md:gap-12'>
+            <ul className='hidden sm:flex text-right font-raleway text-lg gap-16 lining-nums'>
+                <li>+7 343 290 84 76</li>
+                <li>info@66bit.ru</li>
+            </ul>
+            
+            <ToggleSwitch />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
