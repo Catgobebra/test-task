16327561474b24ad9687c80d1b9cc08bbd7302c9
Catgobebra <elderlyjmih@gmail.com>
2026-02-01 12:59:30 +0500

    feat: implement infinite scroll in EmployeeListPage

--- src/pages/EmployeeListPage/components/EmployeeListPage/EmployeeListPage.tsx
+++ src/pages/EmployeeListPage/components/EmployeeListPage/EmployeeListPage.tsx
@@ -1,26 +1,33 @@
 import * as React from 'react'
-import { useQuery } from '@tanstack/react-query'
 import EmployeeListPageContent from '../EmployeeListPageContent/EmployeeListPageContent';
 import Header from '../../../../components/Header/Header/Header';
 import Loading from '../../../../ui/Loading';
 import Error from '../../../../ui/Error';
-import type { EmployeeDetail } from '../../../../types/employee';
-import { fetchEmployeeList } from '../../api/employee';
+import { useEmployeesQuery } from '../../hooks/useEmployeesQuery';
 
 function EmployeeListPage() {
- const {data : employees, isLoading, isError, error} = useQuery<EmployeeDetail[],Error>({
-      queryFn : () => fetchEmployeeList(),
-      queryKey: ['employees'],
-  })
+ const {data : allEmployees = [],
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage} = useEmployeesQuery()
 
   return (
     <>
     <Header />
     {isLoading && <Loading LoadingText="Загрузка списка сотрудников..." />}
     {isError && <Error errorMessage={error?.message} ErrorText="Не удалось загрузить" />}
-    {!isLoading && !isError && employees && <EmployeeListPageContent employees={employees} />} 
+    {allEmployees.length === 0 && <div className="text-center py-10">Сотрудники не найдены</div>}
+    {!isLoading && !isError && allEmployees.length > 0 && <EmployeeListPageContent 
+    fetchNextPage={fetchNextPage}
+    hasNextPage={hasNextPage}
+    isFetchingNextPage={isFetchingNextPage}
+    employees={allEmployees}
+     />} 
     </>
   )
 }
 
-export default EmployeeListPage;
+export default EmployeeListPage
