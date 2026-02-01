28f89219ded36df71160ea026b597c9f6182465e
Catgobebra <elderlyjmih@gmail.com>
2026-02-02 00:49:26 +0500

    feat: implement name search for employee list page

--- src/pages/EmployeeListPage/api/employee.ts
+++ src/pages/EmployeeListPage/api/employee.ts
@@ -3,10 +3,12 @@ const BASE = 'https://frontend-test-api.stk8s.66bit.ru/api/Employee'
 
 export async function fetchEmployeeList(params: EmployeeListParams = {}) {
     const query = new URLSearchParams()
-
     query.set('Page', String(params.Page ?? 1))
     if (params.Count !== undefined) {
         query.set('Count', String(params.Count));
+    }
+    if (params.Name?.trim()) {
+        query.set('Name', params.Name.trim());
     }
 
     const res = await fetch(`${BASE}?${query.toString()}`)
--- src/pages/EmployeeListPage/hooks/useEmployeesQuery.ts
+++ src/pages/EmployeeListPage/hooks/useEmployeesQuery.ts
@@ -1,18 +1,28 @@
 import { useInfiniteQuery } from "@tanstack/react-query"
 import type { EmployeeDetail } from "../../../types/employee"
 import { fetchEmployeeList } from "../api/employee"
-import { PAGE_SIZE } from "../constant/constants"
+import { PAGE_SIZE, TIME_CACHE } from "../constant/constants"
 
-export const useEmployeesQuery = () => {
-    return useInfiniteQuery<EmployeeDetail[], Error, EmployeeDetail[], ['employees'], number>({
-      queryKey: ['employees'],
+interface useEmployeesQueryProps {
+  name ?: string
+}
+
+export const useEmployeesQuery = ({name} : useEmployeesQueryProps = {}) => {
+    return useInfiniteQuery<EmployeeDetail[],
+     Error,
+     EmployeeDetail[],
+     ['employees',string | undefined],
+     number>({
+      queryKey: ['employees',name],
       queryFn: async ({ pageParam = 1 } : {pageParam : number}) => {
         const employees = await fetchEmployeeList({
           Page: pageParam,
           Count: PAGE_SIZE,
+          Name: name,
         })
         return employees
       },
+      staleTime: 1000 * TIME_CACHE,
       initialPageParam: 1,
       getNextPageParam: (lastPage, allPages) => {
         if (lastPage.length < PAGE_SIZE) {
