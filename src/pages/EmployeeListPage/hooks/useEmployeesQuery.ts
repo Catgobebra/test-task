0ba48015d705c083ac0886e70c68c82ecdb4e0dd
Catgobebra <elderlyjmih@gmail.com>
2026-02-03 00:52:04 +0500

    feat: add useEmployeesQuery hook with infinite query + filters support

--- src/pages/EmployeeListPage/hooks/useEmployeesQuery.ts
+++ src/pages/EmployeeListPage/hooks/useEmployeesQuery.ts
@@ -5,20 +5,26 @@ import { PAGE_SIZE, TIME_CACHE } from "../constant/constants"
 
 interface useEmployeesQueryProps {
   name ?: string
+  gender ?: string[]
+  position ?: string[]
+  stack ?: string[]
 }
 
-export const useEmployeesQuery = ({name} : useEmployeesQueryProps = {}) => {
+export const useEmployeesQuery = ({name, gender, position, stack} : useEmployeesQueryProps = {}) => {
     return useInfiniteQuery<EmployeeDetail[],
      Error,
      EmployeeDetail[],
-     ['employees',string | undefined],
+     ['employees',string | undefined,string[] | undefined,string[] | undefined,string[] | undefined],
      number>({
-      queryKey: ['employees',name],
+      queryKey: ['employees',name, gender, position, stack],
       queryFn: async ({ pageParam = 1 } : {pageParam : number}) => {
         const employees = await fetchEmployeeList({
           Page: pageParam,
           Count: PAGE_SIZE,
           Name: name,
+          Gender : gender,
+          Position : position,
+          Stack : stack
         })
         return employees
       },
